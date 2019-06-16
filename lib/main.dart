b85e850a8d519771d6ec57631ac7951f893f0758
unknown <ntanhthu250500@gmail.com>
2019-06-16 17:43:51 +0700

    delete random word

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:english_words/english_words.dart';
 //import 'home.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -14,58 +14,18 @@ class MyApp extends StatelessWidget {
       title: "list of Flutter app",
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("tuyen sinh"),
+          title: new Text("Tuyển Sinh CTU"),
         ),
-        body: RandomWords(),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => 
+            ListTile(title: Text("Item $index"), onTap: () => print('Tapped $index'),)
+         ),
     ),
     );
     }
     
 }
-
-class RandomWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new RandomWordsState();
-  }
-}
-class RandomWordsState extends State<RandomWords> {//qli trang thai 
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-  return new ListView.builder(
-    padding: const EdgeInsets.all(10.0),
-    itemBuilder: (context, i) {
-      if (i.isOdd) return Divider();
-      final index = i ~/ 2;
-      // If you've reached at the end of the available word pairs...
-      if (index >= _suggestions.length) {
-        // ...then generate 10 more and add them to the suggestions list.
-      _suggestions.addAll(generateWordPairs().take(10));
-      }
-        return _buildRow(_suggestions[index]);
-    },
-  );
-}
-    Widget _buildRow(WordPair pair) {
-    return new ListTile(
-      title: new Text(pair.asPascalCase, style: _biggerFont),
-    );
-  }
-
-}
-
-
-
 
 
 
